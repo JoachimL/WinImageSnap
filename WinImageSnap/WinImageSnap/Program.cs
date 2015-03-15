2748e60f05dc4aa56d4b20d9bd56629ea75d3e8b
Joachim Lovf <joachim.lovf@gmail.com>
2015-03-15 23:15:32 +0100

    Initial commit.

---
+++ WinImageSnap/WinImageSnap/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using clipr;
+
+namespace WinImageSnap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = CliParser.Parse<Options>(args);
+            new Snapper(new Configuration()
+            {
+                MaxSleepTime = options.MaxWaitTime,
+                RepositoryName = options.RepositoryName,
+                OutputFolder = options.OutputFolder
+            }).Snap();
+        }
+    }
+
+    [ApplicationInfo(Description = "The options for WinImageSnap.")]
+    public class Options
+    {
+        [NamedArgument('r', "repositoryName", Action = ParseAction.Store,
+            Description = "The name of the repository.")]
+        public string RepositoryName { get; set; }
+
+        [NamedArgument('w', "maxWaitTime", Action = ParseAction.Store,
+            Description = "The maximum time to wait for a snap.")]
+        public int MaxWaitTime { get; set; }
+
+        [NamedArgument('o', "outputFolder", Action = ParseAction.Store,
+            Description = "The output folder (Where snaps are stored).")]
+        public string OutputFolder { get; set; }
+
+
+    }
+}
---
+++ WinImageSnap/WinImageSnap/Snapper.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Configuration;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace WinImageSnap
+{
+    class Snapper
+    {
+        private bool _snapped;
+        private VideoCaptureDevice _cam;
+        private Bitmap _bitmap;
+        private const int SleepTime = 100;
+        private readonly int _maxSleepTime = 2;
+        private readonly string _repositoryName = "";
+        private readonly string _outputFolder = "C:\\temp";
+
+        public Snapper(Configuration config)
+        {
+            if (config.MaxSleepTime > 0)
+                _maxSleepTime = config.MaxSleepTime;
+
+            if (!string.IsNullOrWhiteSpace(config.RepositoryName))
+                _repositoryName = config.RepositoryName;
+
+            if (!string.IsNullOrWhiteSpace(config.OutputFolder))
+                _outputFolder = config.OutputFolder;
+        }
+
+        internal void Snap()
+        {
+            var webcams = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            FilterInfo webcamDevice = webcams[0];
+            _cam = new VideoCaptureDevice(webcamDevice.MonikerString);
+            _cam.NewFrame += CamOnSnapshotFrame;
+            while (_bitmap == null)
+            {
+                _cam.Start();
+                WaitForSnap();
+                _cam.Stop();
+            }
+            SaveBitmap(_bitmap);
+        }
+
+        private void WaitForSnap()
+        {
+            var sleptFor = 0;
+            while (!_snapped && sleptFor < _maxSleepTime * 1000)
+            {
+                Thread.Sleep(SleepTime);
+                sleptFor += SleepTime;
+            }
+        }
+
+        void CamOnSnapshotFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            _bitmap = (Bitmap)eventArgs.Frame.Clone();
+            _snapped = true;
+        }
+
+        private void SaveBitmap(Bitmap bitmap)
+        {
+            AddTextToBitmap(bitmap);
+            if (!Directory.Exists(_outputFolder))
+                Directory.CreateDirectory(_outputFolder);
+            bitmap.Save(Path.Combine(_outputFolder, "snap_" + GetTimestamp() + ".png"), ImageFormat.Png);
+        }
+
+        private void AddTextToBitmap(Bitmap bitmap)
+        {
+            var font = new Font("Courier New", 20, FontStyle.Regular);
+            var graphics = Graphics.FromImage(bitmap);
+
+            if (!string.IsNullOrWhiteSpace(_repositoryName))
+            {
+                SizeF measuredSize = graphics.MeasureString(_repositoryName, font);
+                graphics.DrawString(_repositoryName, font, Brushes.DarkOrange, bitmap.Width - measuredSize.Width, bitmap.Height - measuredSize.Height);
+            }
+            var timestamp = DateTime.Now.ToString("G");
+            graphics.DrawString(timestamp, font, Brushes.DarkOrange, 0, 0);
+        }
+
+        private string GetTimestamp()
+        {
+            return DateTime.Now.ToString("s").Replace(":", "").Replace('T', '_');
+        }
+    }
+}
